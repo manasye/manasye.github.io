d55199a82400127a7649cff1f612029a0ae3e48f
manasyebukit <manasyebukit@gmail.com>
2018-03-24 16:12:55 +0700

    add sliding effect

--- javascript/animations.js
+++ javascript/animations.js
@@ -1,3 +1,23 @@
-$('.navigation__link').on('click',function(){
-    $('.navigation__checkbox').prop('checked',false);
+$(document).ready(function(){
+    $('.btn').on('click',function(e){
+
+        if (this.hash !== ""){
+            e.preventDefault();
+            var hash = this.hash;
+
+            $('html, body').animate(
+                {scrollTop: $(hash).offset().top},
+                500,
+                function(){
+                    window.location.hash = hash;
+                }
+            );
+        }
+
+    })
+
+    $('.navigation__link').on('click',function(){
+        $('.navigation__checkbox').prop('checked',false);
+    });
+
 });
